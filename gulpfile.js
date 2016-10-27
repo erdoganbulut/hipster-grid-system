926af618f1ec6aa1d43ecaf80a0b914ad9eb0caf
erdoganbulut <erdgnblt@gmail.com>
2016-10-27 16:32:03 +0300

    Update gulpfile for sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
 var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task('sass', function () {
+    gulp.src('src/hipster-grid-system.scss')
+        .pipe(sass({
+            // includePaths: require('node-bourbon').with('other/path', 'another/path')
+            // - or -
+            includePaths: require('node-bourbon').includePaths
+        }))
+        .pipe(gulp.dest('dist/hipster-grid-system.css'));
+});
