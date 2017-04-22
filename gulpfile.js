3e5676a945453585b9bc05c3bd22d2aefc678183
Erdoğan Bulut <erdgnblt@gmail.com>
2017-04-22 18:15:09 +0300

    Remove bourbon options for new version

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,6 @@ var sass = require("gulp-sass");
 
 gulp.task('sass', function () {
     gulp.src('src/hipster-grid-system.scss')
-        .pipe(sass({
-            // includePaths: require('node-bourbon').with('other/path', 'another/path')
-            // - or -
-            includePaths: require('node-bourbon').includePaths
-        }))
+        .pipe(sass())
         .pipe(gulp.dest('dist'));
 });
