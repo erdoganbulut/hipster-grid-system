4fcee02539bddc4c2b0079fa6f4e5707a6f887c6
Erdoğan Bulut <erdgnblt@gmail.com>
2017-04-29 22:59:24 +0300

    Add support autoprefixer for cross-browser. Add support minify dist file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var rename = require("gulp-rename");
+var autoprefixer = require("gulp-autoprefixer");
 
 gulp.task('sass', function () {
     gulp.src('src/hipster-grid-system.scss')
         .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('dist'));
+    gulp.src('src/hipster-grid-system.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer())
+        .pipe(rename({
+            suffix: ".min"
+        }))
         .pipe(gulp.dest('dist'));
 });
